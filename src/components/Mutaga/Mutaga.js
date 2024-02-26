15030b055a70135d0077b64180460d3af6931dd9
Mutagah <kihahumutagah@gmail.com>
2024-02-26 21:54:35 +0300

    :lipstick:feat:Creation of Mutaga's page.

---
+++ src/components/Mutaga/Mutaga.js
@@ -0,0 +1,33 @@
+import "./Mutaga.css";
+export default function Mutaga() {
+  return (
+    <>
+      <h1>
+        Page by <i style={{ fontWeight: "lighter" }}>Kihahu Mutaga</i>
+      </h1>
+      <div className="my-container">
+        <div className="title">
+          <h4 className="tag-title">Professional summary</h4>
+        </div>
+        <div className="description">
+          <p>
+            I am a highly punctual, enthusiastic, and detail-oriented individual
+            equipped with excellent interpersonal and communication skills,
+            actively seeking a junior web developer position or an internship.
+            Throughout the recent years, I have acquired extensive knowledge in
+            the IT industry, focusing particularly on software development with
+            a strong command of stacks such as Ruby on Rails and React.
+            Additionally, I have contributed to open-source projects utilizing
+            the Angular framework. Possessing the ability to grasp complex tasks
+            with precision, my ultimate objective is to secure a challenging
+            career in the IT industry, specifically in software development,
+            where I can uphold a first-class standard in my work. My holistic
+            understanding of organizational dynamics and a preference for a
+            conducive working environment positions me well to deliver
+            world-class solutions.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
