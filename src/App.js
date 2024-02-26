e794403cfe3c6656cfd362ea6a4aa2afc2b0e2c1
Mutagah <kihahumutagah@gmail.com>
2024-02-26 20:07:20 +0300

    :sparkles:feat:route configuration.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import Owen from "./components/Owen";
 function App() {
   return (
     <>
       <h1>This is the Git group project exercise</h1>
+
+      {/* Route configuration */}
+      <Routes>
+        <Route path="/Owen" element={<Owen />} />
+      </Routes>
     </>
   );
 }
