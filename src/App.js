3130ea5a3ba6c6c148ec1442ed2202ed39500238
Mutagah <kihahumutagah@gmail.com>
2024-02-26 21:58:31 +0300

    :lipstick:feat:Styling of the App page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Owen from "./components/Owen";
-import Githuba from "./components/Githuba"
+import Githuba from "./components/Githuba";
+import Mutaga from "./components/Mutaga/Mutaga";
 function App() {
+  let navigate = useNavigate();
+  const names = [
+    "Emily",
+    "Stephen",
+    "Michael",
+    "Wesley",
+    "Reuben",
+    "Mutaga",
+    "Stephanie",
+    "Jeremy",
+  ];
+  const mappedButtonNames = names.map((name) => {
+    return (
+      <button className="button-names" onClick={() => navigate(`/${name.toLowerCase()}`)}>
+        {name}
+      </button>
+    );
+  });
   return (
-    <>
-      <h1>This is the Git group project exercise</h1>
-
+    <main className="main-container">
+      <h1>Git group project practice exercise</h1>
+      {mappedButtonNames}
       {/* Route configuration */}
       <Routes>
-        <Route path="/Owen" element={<Owen />} />
-        <Route path="/Githuba" element={<Githuba />} />
+        <Route path="/michael" element={<Owen />} />
+        <Route path="/jeremy" element={<Githuba />} />
+        <Route path="/mutaga" element={<Mutaga />} />
       </Routes>
-    </>
+    </main>
   );
 }
 
