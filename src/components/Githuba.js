2b8a8a5f3b403115576962f40fb18a560e9016b3
Githuba Jeremy <githuba9520@gmail.com>
2024-02-26 19:15:46 +0300

    githuba component

---
+++ src/components/Githuba.js
@@ -0,0 +1,9 @@
+function Githuba (){
+    return (
+        <div className="githuba-container" >
+            <h1>
+                Githuba Richard Jeremy
+            </h1>
+        </div>
+    )
+}
